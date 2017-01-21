7f98d95ba1e7572388739e78ed0c2d95b97d38e0
Konrad <konrad_here@hotmail.com>
2017-01-21 12:34:52 -0800

    update block diagram

--- Project1/Phase1/photoResistorTest/photoResistorTest.ino
+++ Project1/Phase1/photoResistorTest/photoResistorTest.ino
@@ -1,17 +1,8 @@
-#include <LiquidCrystal.h>
-
-// initialize the library with the numbers of the interface pins.
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-
 void setup() {
-  // Initialize pin 53 for logic analyzer timing.
-  pinMode(53, OUTPUT);
-  lcd.begin(16, 2);
+
 }
 
 void loop() {
-  lcd.print("123");
-  delay(1000);
-  lcd.clear();
+
 }
 
