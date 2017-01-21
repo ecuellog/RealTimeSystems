654a032b7c0da60f142fbfdde1f7a1c1ea6e4712
Konrad <konrad_here@hotmail.com>
2017-01-21 13:15:27 -0800

    update block diagram and photoresistor/laser working

--- Project1/Phase1/photoResistorTest/photoResistorTest.ino
+++ Project1/Phase1/photoResistorTest/photoResistorTest.ino
@@ -1,8 +1,20 @@
-void setup() {
+/* 
+ *  Code modified from arduino website.
+ */
 
+int lightSensorPin = 6;
+
+int ambientLight = 0;
+void setup() {
+    Serial.begin(9600);
+    ambientLight = analogRead(lightSensorPin);
 }
 
 void loop() {
+    int light = analogRead(lightSensorPin);
 
+    // light goes up by 100
+    if (light - ambientLight > 100) {
+      Serial.println("SHINY");
+    }
 }
-
