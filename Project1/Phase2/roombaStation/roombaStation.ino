6ccf84e0f8a0dd35f03ec1e9a197d74f789cfe91
Edgardo Cuello <ecuellog@gmail.com>
2017-01-29 17:57:09 -0800

    added roombastation servo and laser BT functionality

--- Project1/Phase2/roombaStation/roombaStation.ino
+++ Project1/Phase2/roombaStation/roombaStation.ino
@@ -1,18 +1,48 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial1.begin(9600);
+#include <Servo.h>
 
+Servo servoX;
+Servo servoY;
+int command;
+int comInfo;
+int curAngleX;
+int curAngleY;
+int laserPin = 53;
+int servoXpin = 52;
+int servoYpin = 50;
+
+void setup() {
+  pinMode(laserPin, OUTPUT);
+  servoX.attach(servoXpin);
+  servoY.attach(servoYpin);
+  Serial1.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available()) {
-    Serial1.print((char)Serial.read());
-  }
+  //get command from base station
+  command = Serial1.read();
 
-  if(Serial1.available()) {
-    Serial.print((char)Serial1.read());
+  switch(command){
+    case 0:
+      curAngleX = Serial1.read();
+      Serial.print(curAngleX); //for testing purposes to protect servo motor
+      delay(500);
+      //servoX.write(curAngleX);
+      break;
+    case 1:  
+      curAngleY = Serial1.read();
+      Serial.print(curAngleY); //for testing purposes to protect servo motor
+      delay(500);
+      //servoY.write(curAngleY);
+      break;
+    case 2:
+      comInfo = Serial1.read();
+      if(comInfo){
+        digitalWrite(laserPin, LOW);
+      } else {
+        digitalWrite(laserPin, HIGH);
+      }
+      break;
   }
-
+  
 }
