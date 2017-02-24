4f716ef8cfb8aaa77fad2fbb55f4ae06de4d44f0
Konrad <konrad_here@hotmail.com>
2017-02-24 01:36:56 -0800

    simplify sampleMake main

--- sampleMake/main.cpp
+++ sampleMake/main.cpp
@@ -1,14 +1,5 @@
-#include <avr/io.h>
 #include <Arduino.h>
 #include <LiquidCrystal.h>
-
-
-#define shift_light_up(port) port << 1
-#define shift_light_down(port) port >> 1
-
-#define register_set_output(register) register = 0b11111111
-#define enable_all(port) port = 0b11111111
-#define disable_all(port) port = 0b00000000
 
 LiquidCrystal lcd(8, 11, 9, 4, 5, 6, 7);
 
