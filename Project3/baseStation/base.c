95720c6165c1eff41671d1e57cdb115f2492915b
Konrad <konrad_here@hotmail.com>
2017-03-29 17:41:05 -0700

    use rtos tasks

--- Project3/baseStation/base.c
+++ Project3/baseStation/base.c
@@ -4,25 +4,72 @@
  */
 
 #include "utility.h"
+#include "os.h"
 
-uint16_t rx, ry; // range from 0-1024
+typedef enum commands { 
+  ROOMBA = 0, 
+  SERVO, 
+  LASER,
+} COMMANDS;
+
 void readJsRoombaTask() {
-  rx = readAdc(8);
-  ry = readAdc(9);
+  uint16_t rx, ry; // value between 0-1024
+
+  for(;;) {
+    rx = readAdc(8);
+    ry = readAdc(9);
+
+    sendBluetooth('<');
+    sendBluetooth(ROOMBA);
+    sendBluetooth(rx >> 8);
+    sendBluetooth(rx & 0xff);
+    sendBluetooth(ry >> 8);
+    sendBluetooth(ry & 0xff);
+    
+    Task_Next();
+  }
 }
 
-uint16_t sx, sy; // range from 0-1024
 void readJsServoTask() {
-  sx = readAdc(10);
-  sy = readAdc(11);
+  uint16_t sx, sy; // value between 0-1024
+
+  for(;;) {
+    sx = readAdc(10);
+    sy = readAdc(11);
+
+    sendBluetooth('<');
+    sendBluetooth(SERVO);
+    sendBluetooth(sx >> 8);
+    sendBluetooth(sx & 0xff);
+    sendBluetooth(sy >> 8);
+    sendBluetooth(sy & 0xff);
+
+    Task_Next();
+  }
 }
 
-uint8_t b; // 0 when pressed
 void readButtonTask() {
-  b = readButton();
+  uint8_t b; // 0 for pressed 1 for open
+  for(;;) {
+    b = readButton();
+    sendBluetooth('<');
+    sendBluetooth(LASER);
+    sendBluetooth(b);
+
+    Task_Next();
+  }
+}
+
+void initialize() {
+  initAdc();
+  initBluetooth();
+  initButton();
+  initLed();
 }
 
 void a_main() {
-  initLed();
-  enableLed();
+  Task_Create_System(initialize, 0);
+  Task_Create_Period(readJsRoombaTask, 0, 10, 1, 0);
+  Task_Create_Period(readJsServoTask, 0, 10, 1, 1);
+  Task_Create_Period(readButtonTask, 0, 10, 1, 2);
 }
