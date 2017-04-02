ced0875935940d550f7c40826b7d545a43fe32c1
Edgardo Cuello <ecuellog@gmail.com>
2017-04-02 05:01:25 -0700

    Added roomba movement and laser functionality according to received bluetooth command values

--- Project3/roombaStation/roomba.c
+++ Project3/roombaStation/roomba.c
@@ -6,11 +6,12 @@
 #include "../common/os.h"
 #include "../common/utility.h"
 #include "roombaUtility.h"
+#include "roombaMotion.h"
 
 int curAngleX;
 int curAngleY;
 
-void servoX(uint8_t joyX) {
+void servoX(int joyX) {
   int angleRateX = (joyX - 513) / 103;
 	
   if( curAngleX < 135 ) {
@@ -24,7 +25,7 @@ void servoX(uint8_t joyX) {
   OCR3C = curAngleX;
 }
 
-void servoY(uint8_t joyY) {
+void servoY(int joyY) {
   int angleRateY = (joyY - 513) / 103;
 
   if( curAngleY < 135 ) {
@@ -38,18 +39,66 @@ void servoY(uint8_t joyY) {
   OCR3B = curAngleY;
 }
 
-void laser(uint8_t laserOn) { }
+void laser(uint8_t laserButton) {
+  if(laserButton) enableLaser();
+  else disableLaser();
+}
 
-void roomba_movement(int speed, int radius) { }
+void roomba_movement(int speed, int radius) {
+  speed = -(speed - 512);
+  int actRad;
+  int actSpeed = speed / 2;
+  
+  if(radius > 900) {
+    actRad = -1;
+    actSpeed = 50;
+  } else if(radius > 800) {
+    actRad = -200;
+  } else if(radius > 700) {
+    actRad = -400;
+  } else if(radius > 600) {
+    actRad = -750;
+  } else if(radius > 530) {
+    actRad = -1500;
+  } else if(radius > 490) {
+    actRad = 32767;
+  } else if(radius > 400) {
+    actRad = 1500;
+  } else if(radius > 300) {
+    actRad = 750;
+  } else if(radius > 200) {
+    actRad = 400;
+  } else if(radius > 100){
+    actRad = 200;
+  } else {
+    actRad = 1;
+    actSpeed = 50;
+  }
+  
+  driveRoomba(actSpeed, actRad);
+}
 
 void checkBluetoothTask() {
+  //for(;;){
   uint8_t command = receiveBluetooth();
+  /* Servo variables */
   uint8_t joyXhigh;
   uint8_t joyXlow;
   uint8_t joyYhigh;
   uint8_t joyYlow;
   int joyX;
   int joyY;
+  
+  /* Laser Variables */
+  uint8_t laserButton;
+  
+  /*Roomba variables */
+  uint8_t speedHigh;
+  uint8_t speedLow;
+  uint8_t radHigh;
+  uint8_t radLow;
+  int speed;
+  int rad;
   
   switch(command) {
     case SERVO:
@@ -63,10 +112,22 @@ void checkBluetoothTask() {
       servoY(joyY);
       break;
     case ROOMBA:
+      speedHigh = receiveBluetooth();
+      speedLow = receiveBluetooth();
+      radHigh = receiveBluetooth();
+      radLow = receiveBluetooth();
+      speed = speedHigh*256 + speedLow;
+      rad = radHigh*256 + radLow;
+      roomba_movement(speed, rad);
       break;
     case LASER:
+      laserButton = receiveBluetooth();
+      laser(laserButton);
       break;
     }
+   // Task_Next();
+   //}
+  
 }
 
 /*
@@ -105,12 +166,17 @@ void initialize() {
   initLaser();
   initBluetooth();
   initServo();
+  initRoombaUart();
+  initRoomba();
 }
 
 void a_main() {
   Task_Create_System(initialize, 0);
   // // voidfuncptr f, int arg, TICK period, TICK wcet, TICK offset
   //Task_Create_Period(checkBluetoothTask, 0, 5, 1, 0);
+  for(;;){
+  	checkBluetoothTask();
+  }
   // Task_Create_Period(checkRoombaTask, 0, 10, 1, 10);
   
 }
